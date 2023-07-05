a12ba83fb7232bc5db3ef32a5ad912178d0176b6
NguyenMinhChien <minhchien03112003@gmail.com>
2023-07-05 23:54:32 +0700

    init feature component

---
+++ src/components/Feature/index.js
@@ -0,0 +1,15 @@
+import classNames from 'classnames/bind'
+import styles from './Feature.module.scss'
+
+
+const cx = classNames.bind(styles);
+
+function Feature() {
+    return ( 
+        <div className={cx('wrapper')}>
+
+        </div>
+     );
+}
+
+export default Feature;
