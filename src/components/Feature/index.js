b0b713a9f94b675d9aa0f13f921d592d6dcbcb0b
NguyenMinhChien <minhchien03112003@gmail.com>
2023-07-07 00:03:45 +0700

    feature component

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -1,13 +1,36 @@
 import classNames from 'classnames/bind'
 import styles from './Feature.module.scss'
 
+import fea1 from '../../images/fea-1.svg'
+import fea2 from '../../images/fea-2.svg'
+
 
 const cx = classNames.bind(styles);
 
 function Feature() {
     return ( 
         <div className={cx('wrapper')}>
+            <div className={cx('feature')}>
+                <div className={cx('feature-item')}>
+                   <img src={fea1} className={cx('img-fea')} alt='fea-img'/>
+                   <div className={cx('info-fea')}>
+                        <span className={cx('price')}>Low Price</span>
+                        <h2 className={cx('title')}>High Coziness</h2>
+                        <span className={cx('sub-title')}>UPTO 50% OFF</span>
+                        <span className={cx('more')}>Explore Items</span>
+                   </div>
+                </div>
 
+                <div className={cx('feature-item')}>
+                   <img src={fea2} className={cx('img-fea')} alt='fea-img'/>
+                   <div className={cx('info-fea')}>
+                        <span className={cx('price')}>Low Price</span>
+                        <h2 className={cx('title')}>High Coziness</h2>
+                        <span className={cx('sub-title')}>UPTO 50% OFF</span>
+                        <span className={cx('more')}>Explore Items</span>
+                   </div>
+                </div>
+            </div>
         </div>
      );
 }
