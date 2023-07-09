9ed0097c4062ab7ed0682b066a30c7e98787d48b
NguyenMinhChien <minhchien03112003@gmail.com>
2023-07-10 02:26:46 +0700

    New Arrival

---
+++ src/components/NewArrival/index.js
@@ -0,0 +1,14 @@
+import classNames from "classnames/bind";
+import styles from './NewArrival.module.scss'
+
+const cx = classNames.bind(styles);
+
+function NewArrival() {
+    return ( 
+        <div className={cx('wrapper')}>
+            
+        </div>
+     );
+}
+
+export default NewArrival;
